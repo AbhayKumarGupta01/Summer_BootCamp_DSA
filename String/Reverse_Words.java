c91590f04981751d7a7d154d4eb450e12a2142c0
Abhay Kumar Gupta <100959648+AbhayKumarGupta01@users.noreply.github.com>
2026-07-05 23:19:28 +0530

    Add files via upload

---
+++ String/Reverse_Words.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class Reverse_Words {
+
+    public static String reverseWords(String s) {
+        StringBuilder ans = new StringBuilder();
+        int i = s.length() - 1;
+
+        while (i >= 0) {
+
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+
+            if (i < 0) {
+                break;
+            }
+
+            int j = i;
+            while (j >= 0 && s.charAt(j) != ' ') {
+                j--;
+            }
+
+            if (ans.length() > 0) {
+                ans.append(" ");
+            }
+
+            ans.append(s.substring(j + 1, i + 1));
+
+            i = j - 1;
+        }
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String s = sc.nextLine();
+
+        String result = reverseWords(s);
+
+        System.out.println("Reversed words:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}
